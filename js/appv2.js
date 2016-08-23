a602192f43a686c5ef9346cb0629eecfd6a1d8c0
aussieyang <yang.j.li91@gmail.com>
2016-08-23 17:50:07 +0800

    scratch that

--- js/appv2.js
+++ js/appv2.js
@@ -60,7 +60,12 @@ function create() {
   stars.enableBody = true;
   //  Here we'll create 12 of them evenly spaced apart
   for (var i = 0; i < 12; i++){
-    createStar()
+    //  Create a star inside of the 'stars' group
+    var star = stars.create(i * 70, 0, 'star');
+    //  Let gravity do its thing
+    star.body.gravity.y = 200;
+    //  This just gives each star a slightly random bounce value
+    star.body.bounce.y = 0.7 + Math.random() * 0.2;
   }
 
   var createStar = function(){
