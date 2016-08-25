c5b91965289ce121ddfa32ba26dda0ed2924f6d5
aussieyang <yang.j.li91@gmail.com>
2016-08-25 16:36:22 +0800

    enemy world collision fixes

--- js/appv2.js
+++ js/appv2.js
@@ -156,7 +156,7 @@ function update() {
   if (enemy3.x > 799){
     enemy3.animations.play('left');
     enemy3.body.velocity.x = -120;
-  } else if (enemy1.x < 5) {
+  } else if (enemy1.x < 21) {
     enemy3.animations.play('right');
     enemy3.body.velocity.x = 120;
   } else {
